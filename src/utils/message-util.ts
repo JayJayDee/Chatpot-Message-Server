9f3e7a49142b940d0b17f6515d846e1e44325de9
JayJayDee <jindongp@gmail.com>
2019-04-05 23:46:39 +0900

    fixed too long message_id generation.

--- src/utils/message-util.ts
+++ src/utils/message-util.ts
@@ -8,12 +8,19 @@ const cipher = (secret: string) =>
   createCipher('des-ede3-cbc', secret);
 
 injectable(UtilModules.Message.CreateMessageId,
-  [ ConfigModules.CredentialConfig ],
-  async (cfg: ConfigTypes.CredentialConfig): Promise<UtilTypes.Message.CreateMessageId> =>
+  [ ConfigModules.CredentialConfig,
+    UtilModules.Auth.DecryptRoomToken,
+    UtilModules.Auth.DecryptMemberToken ],
+  async (cfg: ConfigTypes.CredentialConfig,
+    decryptRoom: UtilTypes.Auth.DecryptRoomToken,
+    decryptMember: UtilTypes.Auth.DecryptMemberToken): Promise<UtilTypes.Message.CreateMessageId> =>
+
     (roomToken, memberToken) => {
       const cp = cipher(cfg.messageSecret);
+      const roomNo = decryptRoom(roomToken).room_no;
+      const memberNo = decryptMember(memberToken).member_no;
       const buffers = [
-        cp.update(`${roomToken}&${memberToken}&${process.hrtime()[1]}`),
+        cp.update(`${roomNo}&${memberNo}&${process.hrtime()[1]}`),
         cp.final()
       ];
       return Buffer.concat(buffers).toString('hex');
